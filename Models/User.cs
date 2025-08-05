c6b89fe63cf50196d6101ce41573ccfd19a0ef5b
dennis <dennis.nilsson1111@gmail.com>
2025-08-05 19:57:02 +0200

    1. setting up Api for User

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last name is required")]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Department is required")]
+        [StringLength(50)]
+        public string Department { get; set; } = string.Empty;
+    }
+}
